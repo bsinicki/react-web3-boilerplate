d83a6a1487771561e9898fa5dbcaaca1e1132150
bsinicki <sinicki.bogdan@gmail.com>
2021-08-13 23:20:59 +0200

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { OrderSide,OrderJSON, OpenSeaAsset, Order} from 'opensea-js/lib/types'
+import BigNumber from 'bignumber.js';
 
 import Web3Modal from 'web3modal';
 // @ts-ignore
@@ -9,9 +11,19 @@ import Wrapper from './components/Wrapper';
 import Header from './components/Header';
 import Loader from './components/Loader';
 import ConnectButton from './components/ConnectButton';
+import { OpenSeaPort, Network } from 'opensea-js';
+const Web3Utils = require('web3-utils');
 
 import { Web3Provider } from '@ethersproject/providers';
 import { getChainData } from './helpers/utilities';
+
+// let seaport:any;
+// let itemInfo = "";
+// let userBalanceOfAsset:any = 0;
+const asset = {
+  tokenAddress: "0xCa26C6ECB3B0b35ed5FD4Cd4680547f3774D525a", // Uniq Collections
+  tokenId: "7", // Token ID
+}
 
 const SLayout = styled.div`
   position: relative;
@@ -48,6 +60,8 @@ const SBalances = styled(SLanding)`
   }
 `;
 
+type tOrders = OrderJSON[];
+
 interface IAppState {
   fetching: boolean;
   address: string;
@@ -57,7 +71,12 @@ interface IAppState {
   pendingRequest: boolean;
   result: any | null;
   electionContract: any | null;
+  asset: OpenSeaAsset | null;
   info: any | null;
+  name: string | null;
+  image: string;
+  isItemOwner: boolean | null;
+  orders: tOrders | any;
 }
 
 const INITIAL_STATE: IAppState = {
@@ -69,7 +88,12 @@ const INITIAL_STATE: IAppState = {
   pendingRequest: false,
   result: null,
   electionContract: null,
-  info: null
+  asset: null,
+  info: null,
+  name: "",
+  image: "https://lh3.googleusercontent.com/DBD8vFv2QZ-ZrJ4jKH2lVwrJxiw4qFw2ntwmT6Ly_MrXoc4HLjZleHprxpfSCPX6_Sw7h-rIFVJ8zluRKqWtFnbvk04NU8nUsxUHdg=s128",
+  isItemOwner: null,
+  orders: []
 };
 
 class App extends React.Component<any, any> {
@@ -114,8 +138,107 @@ class App extends React.Component<any, any> {
     });
 
     await this.subscribeToProviderEvents(this.provider);
+    await this.getOSItem();
 
   };
+
+  public getOSItem = async () => {
+    const address = this.provider.selectedAddress ? this.provider.selectedAddress : this.provider?.accounts[0];
+    const seaport = new OpenSeaPort(this.provider, {
+      networkName: Network.Rinkeby
+    });
+
+    const itemInfo = await seaport.api.getAsset(asset);
+    await this.setState({asset: itemInfo});
+
+    await this.setState({name: itemInfo.name});
+    await this.setState({image: itemInfo.imageUrl});
+    
+    const isOwner = (itemInfo.owner.address === address);
+    
+    await this.setState({isItemOwner: isOwner});
+
+    console.log(this.state.asset);
+  }
+
+  public getSellOrders = async () => {
+    const seaport = new OpenSeaPort(this.provider, {
+      networkName: Network.Rinkeby
+    });
+
+    const {orders} = await seaport.api.getOrders({
+      asset_contract_address: asset.tokenAddress,
+      token_id: asset.tokenId,
+      side: OrderSide.Sell
+    })
+
+    await this.setState({orders: this.state.orders.concat(orders)});
+  }
+
+  public getBuyOrders = async () => {
+    const seaport = new OpenSeaPort(this.provider, {
+      networkName: Network.Rinkeby
+    });
+
+    const {orders} = await seaport.api.getOrders({
+      asset_contract_address: asset.tokenAddress,
+      token_id: asset.tokenId,
+      side: OrderSide.Buy
+    })
+
+    await this.setState({orders: this.state.orders.concat(orders)});
+
+    console.log(this.state.orders);
+  }
+
+  public connectToOpensea  = async ()  => {
+    const address = this.provider.selectedAddress ? this.provider.selectedAddress : this.provider?.accounts[0];
+    
+    const seaport = new OpenSeaPort(this.provider, {
+      networkName: Network.Rinkeby
+    });
+
+    const offer = await seaport.createBuyOrder({
+      asset,
+      accountAddress: address,
+      startAmount: 0.75,
+    })
+
+    console.log(offer);
+  }
+
+  public buyNow  = async ()  => {
+    const address = this.provider.selectedAddress ? this.provider.selectedAddress : this.provider?.accounts[0];
+    
+    const seaport = new OpenSeaPort(this.provider, {
+      networkName: Network.Rinkeby
+    });
+
+    const order:Order = this.state.asset?.sellOrders?.[0]!;
+
+    const transactionHash = await seaport.fulfillOrder({order, accountAddress:address })
+
+    console.log(transactionHash);
+  }
+
+  public sellItemOnOpenSea  = async ()  => {
+    const address = this.provider.selectedAddress ? this.provider.selectedAddress : this.provider?.accounts[0];
+    
+    const seaport = new OpenSeaPort(this.provider, {
+      networkName: Network.Rinkeby
+    });
+
+    const expirationTime = Math.round(Date.now() / 1000 + 60 * 60 * 24)
+
+    const listing = await seaport.createSellOrder({
+      asset,
+      accountAddress: address,
+      startAmount: 0.75,
+      expirationTime
+    })
+
+    console.log(listing);
+  }
 
   public subscribeToProviderEvents = async (provider:any) => {
     if (!provider.on) {
@@ -147,6 +270,7 @@ class App extends React.Component<any, any> {
       await this.resetApp();
     } else {
       await this.setState({ address: accounts[0] });
+      await this.getOSItem();
     }
   }
 
@@ -202,6 +326,7 @@ class App extends React.Component<any, any> {
             killSession={this.resetApp}
           />
           <SContent>
+            
             {fetching ? (
               <Column center>
                 <SContainer>
@@ -211,11 +336,51 @@ class App extends React.Component<any, any> {
             ) : (
                 <SLanding center>
                   {!this.state.connected && <ConnectButton onClick={this.onConnect} />}
+                  {/* <h5>{seaport && seaport}</h5> */}
+                  {this.state.asset &&
+                  <div>
+                    {this.state.isItemOwner&&<h5>You're owner of this item</h5>}
+                  <img
+                    src={this.state.asset.imageUrl}
+                   />
+                   <h2>{this.state.asset.name}</h2>
+                   {this.state.asset.sellOrders && this.state.asset.sellOrders[0] && <div>
+                   <h4>Actual price: {Web3Utils.fromWei(new BigNumber(this.state.asset.sellOrders[0].basePrice).toString(),"ether")} WETH </h4>
+                   {!this.state.isItemOwner&&this.state.asset.sellOrders[0]&&<button onClick={this.buyNow}>
+                    Buy now
+                    </button>}
+                    </div>}
+                  {!this.state.isItemOwner ? 
+                  <button onClick={this.connectToOpensea}>
+                    Create offer
+                  </button>
+                  :
+                  <button onClick={this.sellItemOnOpenSea}>
+                    Sell item
+                  </button>
+                  }
+
+                  <ul>
+                          { this.state.orders.map((item: OrderJSON, index: number) => {
+                            return <li key={index}>
+                              {(item.side===0)?"Buy offer ":"Sell offer "}
+                              {"By: " + item.maker + " "}
+                              {"Price: " + item.basePrice + " "} 
+                              </li>;
+                          })}
+                        </ul>
+
+                        </div>
+                        }
                 </SLanding>
               )}
+
+
+
           </SContent>
         </Column>
       </SLayout>
+
     );
   };
 }
